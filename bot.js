36c0ea07977491e81b6c0c34efb0b7e89d4a451e
LudwigATsl <46196808+LudwigATsl@users.noreply.github.com>
2018-12-30 00:38:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -170,7 +170,6 @@ bot.on("message", message => {
         message.channel.send(PrintEmbed);
         message.channel.send(CGIEmbed);
         message.channel.send(EntwicklungEmbed);
-        break
     
 });
 
